13c3b8ab9776903104211818ffb71732445f4382
farhanpermana <46885640+farhanpermana@users.noreply.github.com>
2023-03-24 11:22:18 +0700

    Initial commit

---
+++ HansGoodsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HansGoodsApp
+//
+//  Created by Farhan on 24/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
